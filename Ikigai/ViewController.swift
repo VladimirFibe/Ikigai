7ee10e932f6f68eda06e09aa3b5c81ee0558e166
Vladimir <motiw@icloud.com>
2022-06-09 06:04:16 +0600

    Initial Commit

---
+++ Ikigai/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ikigai
+//
+//  Created by Икигай on 09.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
